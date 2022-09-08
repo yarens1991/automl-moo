c395f67152ea3f6a603b2a57d828cff956e8b3b0
yarens <yarens.ch@gmail.com>
2022-09-08 10:56:04 +0200

    Heredando

--- automl-moo.py
+++ automl-moo.py
@@ -1,1 +1,10 @@
 '''Project description'''
+
+from autosklearn import *
+from pymoo import *
+
+class Automlmoo(autosklearn, pymoo):
+    pass
+
+if issubclass(Automlmoo, (autosklearn, pymoo)):
+    print('Automlmoo is subclass of autosklearn and pymoo')
